d09ce7c05df1643b20f2b2e2c82fec3f8009ae7a
Robert W <robwash@gmail.com>
2018-03-02 03:51:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -42,7 +42,7 @@ class CashRegister
 
 
   def void_last_transaction
-    @total = @total - @last_transaction
+    self.total = self.total - self.last_transaction
   end
 
 end # => CashRegister End
